e5cd191099d406907c1cf273bb6ab15d99184094
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-05-26 13:02:22 -0300

    feat: estilos index finalizado

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-//const modeDark = document.getElementById('mode-dark')
-
+/*---Toggle Dark Model---*/
 modeDark.addEventListener('click', () => {
    const page = document.querySelector('html')
    page.classList.toggle('dark-mode')
+})
+
+/*--- Back To Top ---*/
+window.addEventListener('scroll', () => {
+   const backToTop = document.querySelector('.back-to-top')
+   if (window.scrollY >= 100) {
+      backToTop.classList.add('show')
+   } else {
+      backToTop.classList.remove('show')
+   }
 })
