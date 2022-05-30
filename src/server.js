3ec73d4a305f139c2a0d07b01586e79d32c64a11
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-05-30 15:12:49 -0300

    feat: criando ejs dos arquivos

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const path = require('path')
+
+const app = express()
 
 app.set('view engine', 'ejs')
+
 app.use(express.static('public'))
-//app.set('views', path.join(__dirname, 'views'))
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-   res.render('pages/index')
+   res.render('pages/index.ejs')
 })
 
-app.listen(8080)
-console.log('Server running in port 8080')
-//start server -> node server
+app.listen(8080, () => console.log('Server running in port 8080'))
+
+// start server -> node server or npm start
